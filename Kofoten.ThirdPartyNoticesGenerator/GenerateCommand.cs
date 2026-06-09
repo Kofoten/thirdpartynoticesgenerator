f2917dac118551a12820513b4483e45a011b4657
Rasmus Steen <rasmus@steen.ninja>
2026-06-10 00:40:54 +0200

    add generate command

---
+++ Kofoten.ThirdPartyNoticesGenerator/GenerateCommand.cs
@@ -0,0 +1,20 @@
+﻿using Kofoten.SimpleCli;
+
+namespace Kofoten.ThirdPartyNoticesGenerator;
+
+public class GenerateCommand : ICliCommand
+{
+    [CliArgument(0, nameof(AssetsPath), Description = "Path to project.assets.json")]
+    public required string AssetsPath { get; init; }
+
+    [CliArgument(1, nameof(OutputPath), Description = "Path to output ThirdPartyNotices.txt")]
+    public required string OutputPath { get; init; }
+
+    [CliArgument(2, nameof(FallbackDirectory), Description = "Directory containing fallback licenses")]
+    public required string FallbackDirectory { get; init; }
+
+    public int Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Kofoten.ThirdPartyNoticesGenerator/Program.cs
+++ Kofoten.ThirdPartyNoticesGenerator/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Kofoten.ThirdPartyNoticesGenerator;
+
+try
+{
+    return GenerateCommandParser.Parse(args).Execute();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Critical failure: {ex.Message}");
+    return 42;
+}
